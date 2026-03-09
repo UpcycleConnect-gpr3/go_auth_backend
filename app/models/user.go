c04f534e54c2af544d1e4dd9be7c224cfc6b222f
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-03-09 21:33:57 +0100

    feat(user): add basic user model

--- app/models/user.go
+++ app/models/user.go
@@ -1,1 +1,14 @@
 package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	Firstname string    `json:"firstname"`
+	Lastname  string    `json:"lastname"`
+	password  string    `db:"password" json:"-"`
+	Email     string    `json:"email"`
+}

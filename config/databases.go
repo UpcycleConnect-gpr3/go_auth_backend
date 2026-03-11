67388954bac254d72a4ade22590cdf8aaef1ecbe
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-03-11 19:11:35 +0100

    feat(db): add DatabaseAuth to make a connection with primary database

--- config/databases.go
+++ config/databases.go
@@ -1,1 +1,19 @@
 package config
+
+import (
+	"authentication_backend/internal"
+	"database/sql"
+	"os"
+)
+
+var DatabaseAuth *sql.DB
+
+func InitDatabase() {
+
+	DatabaseAuth = internal.NewDatabase(
+		os.Getenv("DATABASE_USERNAME"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("DATABASE_NAME"))
+}

7e79aabfeee12f1745f69c01ffe4c734bc1cab82
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-03-11 19:08:44 +0100

    feat(db): add mariadb-driver

--- internal/mariadb-driver.go
+++ internal/mariadb-driver.go
@@ -1,1 +1,30 @@
 package internal
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gofiber/fiber/v3/log"
+)
+
+func NewDatabase(user string, password string, host string, port string, dbname string) *sql.DB {
+	var intPort, errorToConvert = strconv.Atoi(port)
+
+	if errorToConvert != nil {
+		log.Fatal("Error converting port to int")
+	}
+
+	var sqlInfo = fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, password, host, intPort, dbname)
+
+	conn, err := sql.Open("mysql", sqlInfo)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	log.Debugf("(CONFIG) Database Drive Initialized - %s", dbname)
+
+	return conn
+}

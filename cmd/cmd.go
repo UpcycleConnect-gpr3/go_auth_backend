783b9ebbecfef7d0ac93dce13dc72029521f999d
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-03-13 22:39:36 +0100

    refactor(cmd): refactor cmd file

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,8 +1,8 @@
 package cmd
 
 import (
+	"authentication_backend/cmd/database"
 	"authentication_backend/cmd/server"
-	"flag"
 	"fmt"
 	"os"
 
@@ -10,8 +10,6 @@ import (
 )
 
 func Cmd() {
-	startCmd := flag.NewFlagSet("start", flag.ExitOnError)
-	serveCmd := flag.NewFlagSet("server", flag.ExitOnError)
 
 	if len(os.Args) < 2 {
 		fmt.Println("Commande manquante. Utilisation : monexecutable [start|serve]")
@@ -20,16 +18,15 @@ func Cmd() {
 
 	switch os.Args[1] {
 	case "serve":
-		serveCmd.Parse(os.Args[2:])
-		if err := server.Start("dev"); err != nil {
+		if err := server.Start(); err != nil {
 			log.Fatalf("Erreur lors du démarrage du serveur : %v", err)
 		}
-	case "start":
-		startCmd.Parse(os.Args[2:])
-		fmt.Println("Commande 'start' exécutée")
+
+	case "migrate":
+		database.Migrate()
 
 	default:
-		fmt.Println("Commande inconnue. Utilisation : go main [start|serve]")
+		fmt.Println("Commande inconnue. Utilisation : go main serve")
 		os.Exit(1)
 	}
 }

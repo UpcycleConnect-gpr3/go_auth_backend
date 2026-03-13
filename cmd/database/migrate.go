39ba5374cbc7e7ddf481c35939d3156d98af6aa3
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-03-13 22:41:16 +0100

    feat(cmd): add migrate cmd

--- cmd/database/migrate.go
+++ cmd/database/migrate.go
@@ -1,1 +1,46 @@
 package database
+
+import (
+	"authentication_backend/config"
+	"authentication_backend/database"
+	"authentication_backend/internal"
+	"os"
+
+	"github.com/gofiber/fiber/v3/log"
+	"github.com/joho/godotenv"
+)
+
+func initialize() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Erreur lors du chargement du fichier .env : %v", err)
+	}
+
+	switch os.Getenv("APP_DEBUG") {
+	case "true":
+		log.SetLevel(log.LevelDebug)
+	case "false":
+		log.SetLevel(log.LevelInfo)
+	}
+
+	// Config Initialization
+	config.InitDatabase()
+
+	err = database.Auth.Ping()
+
+	if err != nil {
+		log.Fatalf("(DATABASE) %v", err)
+	}
+
+	internal.CreateTableMigrations(database.Auth)
+
+}
+
+func Migrate() {
+
+	initialize()
+
+	internal.Migrate(database.Auth)
+
+}

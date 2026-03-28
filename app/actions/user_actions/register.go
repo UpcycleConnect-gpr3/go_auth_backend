ece6f3b42aa8301acf457ef60229d3879dac554d
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-03-29 00:06:53 +0100

    refactor(action): refactor action to register a new user

--- app/actions/user_actions/register.go
+++ app/actions/user_actions/register.go
@@ -2,71 +2,37 @@ package user_actions
 
 import (
 	"authentication_backend/app/models/user_models"
-	"authentication_backend/utils/log"
 	"authentication_backend/utils/rules"
-	"encoding/json"
-	"net/http"
 )
 
-func createValidateUser(userDto user_models.Credentials) []string {
-	var errsMsg []string
+func createValidateUser(userDto user_models.Credentials) []rules.ValidationError {
+	var errs []rules.ValidationError
 
-	rules.StringMinLength(userDto.Email, 5, "email", &errsMsg)
-	rules.StringMinLength(userDto.Password, 6, "password", &errsMsg)
+	rules.StringMinLength(userDto.Email, 5, "email", &errs)
+	rules.StringMinLength(userDto.Password, 6, "password", &errs)
+	rules.StringMaxLength(userDto.Password, 30, "password", &errs)
+	rules.MustContainsAny(userDto.Password, "!@#$%^&*()", 1, "password", &errs)
 
-	/*existing, err := models.GetUserByEmail(userDto.Email)
-
-	if err != nil {
-		errsMsg = append(errsMsg, "Error getting User by username")
+	existing := user_models.GetUserByEmail(userDto.Email)
+	if existing != nil {
+		errs = append(errs, rules.ValidationError{
+			Field:   "email",
+			Message: "email must be unique",
+		})
 	}
 
-	if existing != nil {
-		errsMsg = append(errsMsg, "Username must be unique")
-	}*/
-
-	return errsMsg
+	return errs
 }
 
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
+func CreateUser(userDto user_models.Credentials) []rules.ValidationError {
 
-	var userDto user_models.Credentials
+	validationError := createValidateUser(userDto)
 
-	err := json.NewDecoder(r.Body).Decode(&userDto)
-	log.Info(userDto.Email)
-	log.Info(userDto.Password)
-	if err != nil {
-		http.Error(w, "Incorrect body format", http.StatusBadRequest)
-		return
+	if len(validationError) > 0 {
+		return validationError
 	}
 
-	errMsg := createValidateUser(userDto)
+	user_models.CreateUser(userDto)
 
-	if len(errMsg) > 0 {
-		encoded, _ := json.Marshal(errMsg)
-		http.Error(w, string(encoded), http.StatusBadRequest)
-		return
-	}
-
-	log.Api(r)
-
-	err = user_models.CreateUser(userDto)
-
-	if err != nil {
-		log.Info(err.Error())
-		http.Error(w, "pb insertion", http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
-
-	/*	token, err := jwt.GenerateJWT(existing.Id.String())
-		if err != nil {
-			http.Error(w, "Error generating token", http.StatusInternalServerError)
-			return
-		}
-
-		tokenResponse := TokenResponse{BearerToken: token}
-		encodedToken, _ := json.Marshal(tokenResponse)
-		fmt.Fprintf(w, "%s", encodedToken)*/
+	return nil
 }

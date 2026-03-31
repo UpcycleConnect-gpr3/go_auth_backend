f7d2daaa94fedaeeaf0bf413d86552718e862d9b
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-03-31 22:27:12 +0200

    fix(middleware): fix container middleware

--- app/middleware/source_middleware/container.go
+++ app/middleware/source_middleware/container.go
@@ -10,11 +10,12 @@ func Container(allowedContainer string) func(http.HandlerFunc) http.HandlerFunc 
 		return func(w http.ResponseWriter, r *http.Request) {
 			clientContainer := r.Header.Get("X-Container-Name")
 			if clientContainer == "" {
-				clientContainer = r.RemoteAddr
+				log.ApiCodeStatus(w, http.StatusForbidden, "", nil)
+				return
 			}
 
 			if clientContainer != allowedContainer {
-				log.ApiCodeStatus(w, http.StatusForbidden, "Forbidden: Container not allowed", nil)
+				log.ApiCodeStatus(w, http.StatusForbidden, "", nil)
 				return
 			}
 			next.ServeHTTP(w, r)

5ccd0d4e147afac75536d8dbec860c273da48722
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-04-11 21:54:30 +0200

    remove comment

--- app/models/user_models/user.go
+++ app/models/user_models/user.go
@@ -22,7 +22,7 @@ type User struct {
 	Lastname    string    `json:"lastname"`
 	password    string    `db:"password" json:"-"`
 	Email       string    `json:"email"`
-	TOTPSecret  string    `json:"-"` // Secret TOTP (base32)
+	TOTPSecret  string    `json:"-"`
 	TOTPEnabled bool      `json:"totp_enabled"`
 	CreatedAt   time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt   time.Time `db:"updated_at" json:"updated_at"`

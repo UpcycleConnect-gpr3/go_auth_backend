a82fd308559c1be8a8b8083e5753b740bb9e772e
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-04-11 21:57:42 +0200

    feat(totp): add TOTP struct

--- app/models/totp_models/totp.go
+++ app/models/totp_models/totp.go
@@ -3,3 +3,10 @@ package totp_models
 const (
 	TABLE = "TOTPS"
 )
+
+type TOTP struct {
+	ID        int       `json:"id"`
+	Hash      string    `json:"hash"`
+	CreatedAt time.Time `json:"created_at"`
+	UserID    string    `json:"user_id"`
+}

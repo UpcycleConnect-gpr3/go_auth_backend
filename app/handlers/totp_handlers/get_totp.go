fb54bbb378294549631589abfbb024ae224263f9
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-04-12 19:35:59 +0200

    feat(totp): add get handler

---
+++ app/handlers/totp_handlers/get_totp.go
@@ -0,0 +1,29 @@
+package totp_handlers
+
+import (
+	"authentication_backend/app/actions/totp_actions"
+	"authentication_backend/app/models/user_models"
+	"authentication_backend/utils/log"
+	"authentication_backend/utils/response"
+	"net/http"
+)
+
+func GetTOTP(w http.ResponseWriter, r *http.Request) {
+	log.Api(r)
+
+	userID := r.PathValue("userId")
+
+	user := user_models.GetUserByID(userID)
+	if user == nil {
+		response.NewErrorMessage(w, response.ErrUserNotFound, http.StatusNotFound)
+		return
+	}
+
+	totpURL, err := totp_actions.GenerateAndStoreTOTP(user)
+	if err != nil {
+		response.NewErrorMessage(w, response.ErrGenerateTOTP, http.StatusInternalServerError)
+		return
+	}
+
+	response.NewSuccessData(w, map[string]string{"totp_url": totpURL}, response.SuccessGenerateTOTP)
+}

5233b30cf3c6f7d39e4f7868d8ce58a299f28968
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-04-14 22:02:05 +0200

    refactor(user): refactor Login Action

--- app/actions/user_actions/login.go
+++ app/actions/user_actions/login.go
@@ -1,9 +1,15 @@
 package user_actions
 
 import (
+	"authentication_backend/app/models/totp_models"
 	"authentication_backend/app/models/user_models"
+	"authentication_backend/utils/jwt"
 	"authentication_backend/utils/response"
 	"authentication_backend/utils/rules"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"time"
 )
 
 func loginValidateCredential(userDto user_models.Credentials) ([]rules.ValidationError, *user_models.User) {
@@ -13,7 +19,7 @@ func loginValidateCredential(userDto user_models.Credentials) ([]rules.Validatio
 	rules.StringMinLength(userDto.Password, 6, "password", &errs)
 	rules.StringMaxLength(userDto.Password, 30, "password", &errs)
 
-	existing := user_models.GetUserByEmail(userDto.Email)
+	existing := user_models.GetUserByEmailAuth(userDto.Email)
 	if existing == nil {
 		errs = append(errs, rules.ValidationError{
 			Field:   "email",
@@ -34,13 +40,28 @@ func loginValidateCredential(userDto user_models.Credentials) ([]rules.Validatio
 	return errs, existing
 }
 
-func Login(credentials user_models.Credentials) ([]rules.ValidationError, *user_models.User) {
+func Login(credentials user_models.Credentials) (string, bool, []rules.ValidationError, *user_models.User) {
 
-	validationError, existing := loginValidateCredential(credentials)
-
-	if len(validationError) > 0 {
-		return validationError, existing
+	validationErrors, existing := loginValidateCredential(credentials)
+	if len(validationErrors) > 0 {
+		return "", false, validationErrors, existing
 	}
 
-	return validationError, existing
+	if existing.TOTPEnabled {
+
+		timestamp := time.Now().Unix()
+		hashInput := fmt.Sprintf("%s:%d:%s", existing.Id.String(), timestamp, jwt.PrivateKey)
+		hash := sha256.Sum256([]byte(hashInput))
+		hashStr := hex.EncodeToString(hash[:])
+
+		if err := totp_models.CreateTOTPHash(existing.Id.String(), hashStr); err != nil {
+			return "", false, []rules.ValidationError{
+				{Field: "totp", Message: response.ErrGenerateTOTP},
+			}, existing
+		}
+
+		return hashStr, true, nil, existing
+	}
+
+	return "", false, nil, existing
 }

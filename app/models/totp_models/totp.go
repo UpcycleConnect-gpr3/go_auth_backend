c7981404f351fc9f628db9210ddfc888c1bea211
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-04-14 22:04:54 +0200

    feat(totp): Add model methode

--- app/models/totp_models/totp.go
+++ app/models/totp_models/totp.go
@@ -1,6 +1,8 @@
 package totp_models
 
 import (
+	"authentication_backend/database"
+	"database/sql"
 	"time"
 )
 
@@ -16,4 +18,33 @@ type TOTP struct {
 }
 type TOTPCodeRequest struct {
 	Code string `json:"code"`
+}
+
+func CreateTOTPHash(userID, hash string) error {
+	_, err := database.Auth.Exec(
+		"INSERT INTO "+TABLE+" (hash, user_id) VALUES (?, ?)",
+		hash, userID,
+	)
+	return err
+}
+
+func GetTOTPByHash(hash string) (*TOTP, error) {
+	var totp TOTP
+	row := database.Auth.QueryRow(
+		"SELECT id, hash, user_id FROM "+TABLE+" WHERE hash = ?",
+		hash,
+	)
+	err := row.Scan(&totp.ID, &totp.Hash, &totp.UserID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &totp, nil
+}
+
+func DeleteTOTPByHash(hash string) error {
+	_, err := database.Auth.Exec("DELETE FROM "+TABLE+" WHERE hash = ?", hash)
+	return err
 }

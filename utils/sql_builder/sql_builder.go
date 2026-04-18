8ec0d54cfa6ebd0f0357728e1444c331039061fd
FortyTwo-Dev <91018423+FortyTwo-Dev@users.noreply.github.com>
2026-04-18 17:45:55 +0200

    feat(db): add Select many resource function

--- utils/sql_builder/sql_builder.go
+++ utils/sql_builder/sql_builder.go
@@ -13,6 +13,14 @@ func SelectQuery(table string, columns []string, whereClause string) string {
 	return fmt.Sprintf("SELECT %s FROM %s WHERE %s", cols, table, whereClause)
 }
 
+func SelectsQuery(table string, columns []string) string {
+	cols := "*"
+	if len(columns) > 0 {
+		cols = strings.Join(columns, ", ")
+	}
+	return fmt.Sprintf("SELECT %s FROM %s", cols, table)
+}
+
 func InsertQuery(table string, columns []string) string {
 	placeholders := strings.Repeat("?, ", len(columns))
 	placeholders = strings.TrimSuffix(placeholders, ", ")
